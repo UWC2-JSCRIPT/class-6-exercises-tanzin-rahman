5b2ec3167c7abf3d0dada5de49015d71a25e77d8
Tanzin Rahman <tanzin.rahman@gmail.com>
2022-12-02 20:55:17 -0800

    temp code

--- jQueryDomCrud.js
+++ jQueryDomCrud.js
@@ -1,15 +1,87 @@
-// Create a new <a> element containing the text "Buy Now!"
+
+$(document).ready(function X(){
+
+//const main = document.getElementsByTagName('main')[0];
+
+// Create a new <a> element containing the text "Buy Now!" 
 // with an id of "cta" after the last <p>
+// const main = $('main')
+// const a = $('<a>');
+// a.attr('id', 'cta');
+// a.text('Buy Now!');
+// main.append = a;
 
-// Access (read) the data-color attribute of the <img>,
-// log to the console
+// const main = document.getElementsByTagName('main')[0];
 
-// Update the third <li> item ("Turbocharged"),
-// set the class name to "highlight"
+// // Create a new <a> element containing the text "Buy Now!"
+// // with an id of "cta" after the last <p>
+// // Access (read) the data-color attribute of the <img>,
+// // log to the console
+// const img = document.getElementsByTagName('img')[0];
+// console.log(img.dataset.color);
 
-// Remove (delete) the last paragraph
-// (starts with "Available for purchase now…")
+// // Update the third <li> item ("Turbocharged"),
+// // set the class name to "highlight"
 
-// Create a listener on the "Buy Now!" link that responds to a click event.
-// When clicked, the the "Buy Now!" link should be removed
-// and replaced with text that says "Added to cart"
+// const liEls = document.getElementsByTagName('li');
+// const li3 = liEls[2];
+// li3.className = 'highlight';
+
+// // Remove (delete) the last paragraph
+// // (starts with "Available for purchase now…")
+
+// const pEls = document.getElementsByTagName('p');
+// const lastP = pEls[pEls.length-1];
+// main.removeChild(lastP);
+
+// // Create a listener on the "Buy Now!" link that responds to a click event.
+// // When clicked, the the "Buy Now!" link should be removed
+// // and replaced with text that says "Added to cart"
+
+// const a = document.getElementById('cta')
+// a.addEventListener('click', e=>{
+//     a.innerText = "Added to cart";
+// })
+
+// // Create a new <a> element containing the text "Buy Now!" 
+// // with an id of "cta" after the last <p>
+
+// const cta = document.createElement('a');
+// cta.innerText = 'Buy Now!';
+// cta.id = 'cta';
+
+// const main = document.getElementsByTagName('main')[0];
+
+// // Create a new <a> element containing the text "Buy Now!"
+// // with an id of "cta" after the last <p>
+
+// // Access (read) the data-color attribute of the <img>,
+// // log to the console
+// const $img = $('img');
+// const imgColor = $img.data('color') 
+// console.log(imgColor);
+
+// // Update the third <li> item ("Turbocharged"),
+// // set the class name to "highlight"
+
+// const liEls = document.getElementsByTagName('li');
+// const li3 = liEls[2];
+// li3.className = 'highlight';
+
+// // Remove (delete) the last paragraph
+// // (starts with "Available for purchase now…")
+
+// const pEls = document.getElementsByTagName('p');
+// const lastP = pEls[pEls.length-1];
+// main.removeChild(lastP);
+
+// // Create a listener on the "Buy Now!" link that responds to a click event.
+// // When clicked, the the "Buy Now!" link should be removed
+// // and replaced with text that says "Added to cart"
+
+// const a = document.getElementById('cta')
+// a.addEventListener('click', e=>{
+//     a.innerText = "Added to cart";
+// });
+
+})
